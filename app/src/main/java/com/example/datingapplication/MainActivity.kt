606ac913c624b19c44d6a2ee5d99725c5801c10b
Stepan <esastep20@gmail.com>
2026-06-15 11:52:38 +0300

    refactor: remove standard greeting ui element

--- app/src/main/java/com/example/datingapplication/MainActivity.kt
+++ app/src/main/java/com/example/datingapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -20,28 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             DatingApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DatingApplicationTheme {
-        Greeting("Android")
     }
 }
