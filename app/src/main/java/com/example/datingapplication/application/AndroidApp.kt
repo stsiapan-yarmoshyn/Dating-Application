583f50b33738b60a0d25ef1daeb3e57fe12ed922
Stepan <esastep20@gmail.com>
2026-07-30 11:41:42 +0300

    ref: remove commit

--- app/src/main/java/com/example/datingapplication/application/AndroidApp.kt
+++ app/src/main/java/com/example/datingapplication/application/AndroidApp.kt
@@ -3,10 +3,6 @@ package com.example.datingapplication.application
 import android.app.Application
 import di.initKoin
 import org.koin.android.ext.koin.androidContext
-
-//@HiltAndroidApp
-//class App : Application()
-
 class AndroidApp: Application() {
     override fun onCreate() {
         super.onCreate()
